0b292f90c630d756c27af6dcbc8f9ec55767be6e
GaliAkshatha <156352345+GaliAkshatha@users.noreply.github.com>
2026-06-30 16:41:25 +0530

    feat(auth): implement registration service workflow

---
+++ backend/modules/auth/services/authService.js
@@ -0,0 +1,216 @@
+import User from "../../users/models/User.js";
+import organizationService from "../../organization/services/organizationService.js";
+
+import {
+
+    ConflictError,
+
+    BadRequestError,
+
+    NotFoundError,
+
+} from "../../../shared/errors";
+
+import {
+
+    hashPassword,
+
+    comparePassword,
+
+} from "../utils/password.js";
+
+import {
+
+    generateAccessToken,
+
+} from "../utils/jwt.js";
+import { USER_ROLE, USER_STATUS } from "../../users/constants/userConstants.js";
+
+class AuthService {
+
+    async register(registrationData) {
+
+        const organization =
+            await this.getOrganization(
+                registrationData.organizationCode
+            );
+
+        this.validateRegistrationPolicy(
+
+            organization
+
+        ); 
+        
+        this.validateEmailDomain(
+
+            organization,
+
+            registrationData.email
+
+        );
+
+        await this.ensureUserDoesNotExist(
+
+            registrationData.email
+
+        );
+
+        const hashedPassword =
+
+            await hashPassword(
+
+                registrationData.password
+
+            );
+
+        return await this.createUser(
+
+            registrationData,
+
+            organization,
+
+            hashedPassword
+
+        );
+    }
+
+    async getOrganization( organizationCode ){
+
+        const organization =
+
+            await organizationService
+
+            .getOrganizationByCode(
+
+                organizationCode
+
+            );
+
+        if(!organization){
+
+            throw new NotFoundError(
+
+                "Organization not found."
+
+            );
+
+        }
+
+        return organization;
+
+    }
+
+    validateRegistrationPolicy( organization ){
+
+        if(
+
+            !organization.settings?.allowStudentRegistration
+
+        ){
+
+            throw new BadRequestError(
+
+                "Student registration is disabled."
+
+            );
+
+        }
+
+    }
+    
+    validateEmailDomain( organization, email ){
+
+        const domain =
+
+            email
+
+            .split("@")[1]
+
+            .toLowerCase();
+
+        const valid =
+
+            organization.emailDomains.some(
+                allowedDomain => 
+                    allowedDomain.toLowerCase() === domain
+            );
+
+        if(!valid){
+
+            throw new BadRequestError(
+
+                "Email domain is not allowed."
+
+            );
+        }
+    }
+
+    async ensureUserDoesNotExist( email ){
+
+        const user =
+
+            await User.findOne({
+
+                email
+
+            });
+
+        if(user){
+
+            throw new ConflictError(
+
+                "Email already registered."
+
+            );
+
+        }
+
+    }
+
+    async createUser( registrationData, organization, hashedPassword ){
+
+        const user = await User.create({
+
+            organization:
+
+                organization._id,
+
+            name:
+
+                registrationData.name,
+
+            email:
+
+                registrationData.email,
+
+            auth:{
+
+                password:
+
+                    hashedPassword,
+
+            },
+
+            role:USER_ROLE.STUDENT,
+
+            status: USER_STATUS.PENDING,
+
+        });
+
+        return await user.findById(
+            user._id
+        )
+        .populate(
+            "organization",
+            "name code"
+        );
+
+    }
+
+    async login(credentials) {
+
+    }
+
+}
+
+export default new AuthService();
